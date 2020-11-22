51f6148eed8f8bcea1115d80d22b5a1ff3e74737
ksh3760 <ksh3760@naver.com>
2020-11-23 02:40:36 +0900

    update java file

--- 달팽이레이싱게임/src/달팽이레이싱게임/sFrame.java
+++ 달팽이레이싱게임/src/달팽이레이싱게임/sFrame.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class sFrame extends JFrame {
 	// 달팽이 레이블
@@ -15,14 +17,23 @@ public class sFrame extends JFrame {
 	static JLabel label2;
 	static JLabel label3;
 	// 달팽이 x좌표
-	static int x1 = 100, 
-			   x2 = 100, 
-			   x3 = 100;
+	static int x1 = 50, 
+			   x2 = 50, 
+			   x3 = 50;
 	
-	BufferedImage img = null; // 필드 이미지
+	// 버튼
+	JButton bRecord;	// 기록 보기
+	JButton start;		// 게임 시작
+	JButton bCash;		// 현재금액 조회
 	
+	// 현재금액
+	JLabel currentCash = new JLabel("현재금액"); 
+	JTextField fCash = new JTextField(10);
 	
-
+	// 버튼 생성 후 배치
+	
+	// DB연결
+	
 	
 	// 달팽이 게임
 	public sFrame() {
@@ -34,26 +45,29 @@ public class sFrame extends JFrame {
 			
 			label1 = new JLabel();
 			label1.setIcon(new ImageIcon("a.gif"));
+			add(label1);
 			
 			label2 = new JLabel();
 			label2.setIcon(new ImageIcon("b.gif"));
+			add(label2);
 			
 			label3 = new JLabel();
-			label3.setIcon(new ImageIcon("c.gif"));
-			
-			add(label1);
-			add(label2);
+			label3.setIcon(new ImageIcon("c.gif"));		
 			add(label3);
 			
 			// 오른쪽에 정렬
-			label1.setBounds(100, 0, 100, 100);	
-			label2.setBounds(100, 50, 100, 100);
-			label3.setBounds(100, 100, 100, 100);
+//			label1.setBounds(100, 0, 100, 100);	
+//			label2.setBounds(100, 50, 100, 100);
+//			label3.setBounds(100, 100, 100, 100);
+			
+			
 			
 // -----------------------------------------------------------------
 
 			
 			
+			
+			// 스레드 실행
 			(new sThread()).start();
 			setVisible(true);
 	
--- 달팽이레이싱게임/src/달팽이레이싱게임/sThread.java
+++ 달팽이레이싱게임/src/달팽이레이싱게임/sThread.java
@@ -3,20 +3,20 @@ package 달팽이레이싱게임;
 public class sThread extends Thread {
 	
 	public void run() {
-		for(int i = 0 ; i < 120 ; i++) {
+		for(int i = 0 ; i < 300 ; i++) {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			
-			sFrame.x1 += (int)(Math.random() * 10);	// 0.1초에 한번씩 변경
+			sFrame.x1 += (int)(Math.random() * 5);	// 0.1초에 한번씩 변경
 			sFrame.label1.setBounds(sFrame.x1, 0, 100, 100);
 			
-			sFrame.x2 += (int)(Math.random() * 10);
+			sFrame.x2 += (int)(Math.random() * 5);
 			sFrame.label2.setBounds(sFrame.x2, 50, 100, 100);
 			
-			sFrame.x3 += (int)(Math.random() * 10);
+			sFrame.x3 += (int)(Math.random() * 5);
 			sFrame.label3.setBounds(sFrame.x3, 100, 100, 100);
 		}
 	}
